369bbc08c48e2a9dfc3b987f3adb1c588f09b8d7
Alex Platt <alja.platt@gmail.com>
2022-01-14 13:04:42 +0000

    header + styling

---
+++ src/Header/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header class="intro">
+      <div className="container">
+        <h1 class="intro__title">The KSA Movie Database</h1>
+        <p class="intro__text">
+          The <em>only</em> Movie Database you need.
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
